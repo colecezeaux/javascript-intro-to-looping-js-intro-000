50c77188a5f2d04113b97e3ecfc6fe6af3896d81
Cole Cezeaux <colecezeaux@gmail.com>
2020-09-24 20:56:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,27 +1,10 @@
 function forLoop(array) {
-  for (let i=0; i<25; i++) {
-   if(i===1) {
-     array.push("I am 1 strange loop.")
-   } else {
-     array.push(`I am ${i} strange loops.`)
-   }
-}
-return array
-}
-
-function whileLoop(n) {
-  while (n > 0) {
-  console.log(n);
-  n-=1
-}
-return "done"
-}
-
-function doWhileLoop(num) {
-  let i = 0;
-  do {
-  console.log("I run once regardless.")
-        i++
-}
-while (i<num);
+  for(i=0; i<25; i++) {
+    if(i==1) {
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push(`I am ${i} strange loops.`)
+    }
+  }
+  return array
 }
